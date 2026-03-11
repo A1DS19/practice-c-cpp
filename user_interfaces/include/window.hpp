dc3b9549e0f4f4db21c7564772e5069693585ee3
Jose Padilla <jp06171@gmail.com>
2026-03-11 01:19:53 -0600

    rename, delete, open files

--- user_interfaces/include/window.hpp
+++ user_interfaces/include/window.hpp
@@ -19,6 +19,15 @@ public:
 private:
     fs::path current_directory_;
     fs::path selected_entry_;
+    static constexpr std::string_view RENAME_FILE_POPUP_ID = "Rename file";
+    static constexpr std::string_view DELETE_FILE_POPUP_ID = "Delete file";
+    bool rename_popup_open_ = false;
+    bool delete_popup_open_ = false;
+    void open_file_default_editor();
+    void rename_file_popup();
+    void delete_file_popup();
+    bool rename_file(const fs::path& old_path, const fs::path& new_path);
+    bool delete_file(const fs::path& path);
 };
 
 } // namespace UI
--- user_interfaces/src/window.cpp
+++ user_interfaces/src/window.cpp
@@ -1,13 +1,15 @@
 #include "window.hpp"
 
 #include "imgui.h"
-#include "imgui_internal.h"
 #include "main.hpp"
 
 #include <cstddef>
+#include <cstdlib>
 #include <cstring>
+#include <exception>
 #include <filesystem>
 #include <format>
+#include <iostream>
 #include <string_view>
 
 using namespace UI;
@@ -67,7 +69,36 @@ void Window::draw_content() {
     }
 }
 
-void Window::draw_actions() {}
+void Window::draw_actions() {
+    if (fs::is_directory(selected_entry_)) {
+        ImGui::Text("Selected dir: %s", selected_entry_.string().c_str());
+    } else if (fs::is_regular_file(selected_entry_)) {
+        ImGui::Text("Selected file: %s", selected_entry_.string().c_str());
+    } else {
+        ImGui::Text("Nothing selected");
+    }
+
+    if (fs::is_regular_file(selected_entry_)) {
+        if (ImGui::Button("Open file")) {
+            open_file_default_editor();
+        }
+    }
+
+    if (ImGui::Button("Rename")) {
+        rename_popup_open_ = true;
+        ImGui::OpenPopup(RENAME_FILE_POPUP_ID.data());
+    }
+
+    ImGui::SameLine();
+
+    if (ImGui::Button("Delete")) {
+        delete_popup_open_ = true;
+        ImGui::OpenPopup(DELETE_FILE_POPUP_ID.data());
+    }
+
+    rename_file_popup();
+    delete_file_popup();
+}
 
 void Window::draw_filters() {
     static char extension_filter[16] = {"\0"};
@@ -97,4 +128,78 @@ void Window::draw_filters() {
     }
 
     ImGui::Text("Number of files: %zu", filtered_file_count);
+}
+
+void Window::open_file_default_editor() {
+#ifdef _WIN32
+    const auto COMMAND = "start \"\" \"" + selected_entry_.string() + "\"";
+#elif __APPLE__
+    const auto COMMAND = "open \"" + selected_entry_.string() + "\"";
+#else
+    const auto COMMAND = "xdg-open \"" + selected_entry_.string() + "\"";
+#endif
+
+    std::system(COMMAND.c_str());
+}
+
+void Window::rename_file_popup() {
+    if (ImGui::BeginPopupModal(RENAME_FILE_POPUP_ID.data(), &rename_popup_open_,
+                               ImGuiWindowFlags_NoResize)) {
+        static char file_name_buff[512] = "\0";
+        ImGui::Text("New name: ");
+        ImGui::InputText("###new_file_name", file_name_buff, sizeof(file_name_buff));
+
+        if (ImGui::Button("Save")) {
+            auto new_path = selected_entry_.parent_path() / file_name_buff;
+            if (rename_file(selected_entry_, new_path)) {
+                selected_entry_ = new_path;
+                ImGui::CloseCurrentPopup();
+            }
+        }
+
+        ImGui::EndPopup();
+    }
+}
+
+void Window::delete_file_popup() {
+    if (ImGui::BeginPopupModal(DELETE_FILE_POPUP_ID.data(), &delete_popup_open_,
+                               ImGuiWindowFlags_NoResize)) {
+
+        ImGui::Text("Are you sure?");
+
+        if (ImGui::Button("Yes")) {
+            if (delete_file(selected_entry_)) {
+                selected_entry_.clear();
+                ImGui::CloseCurrentPopup();
+            }
+        }
+
+        ImGui::SameLine();
+
+        if (ImGui::Button("No")) {
+            ImGui::CloseCurrentPopup();
+        }
+
+        ImGui::EndPopup();
+    }
+}
+
+bool Window::rename_file(const fs::path& old_path, const fs::path& new_path) {
+    try {
+        fs::rename(old_path, new_path);
+        return true;
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << '\n';
+        return false;
+    }
+}
+
+bool Window::delete_file(const fs::path& path) {
+    try {
+        fs::remove(path);
+        return true;
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << '\n';
+        return false;
+    }
 }
